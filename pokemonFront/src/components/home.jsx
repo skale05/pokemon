7dae15cec51513a3c5f8833989a19aec3e8303b6
Marie <helleliv@Air-von-Marie.fritz.box>
2023-07-06 15:44:52 +0200

    added CSS

--- pokemonFront/src/components/home.jsx
+++ pokemonFront/src/components/home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import PokemonDetails from "./PokemonDetails";
+import "./Home.css";
 
 function Home() {
   const [pokemon, setPokemon] = useState([]);
@@ -18,23 +19,30 @@ function Home() {
   }, []);
 
   return (
-    <div>
-      <Link to={`/BattleGame`}>
-        <button>Go to BattleGame page</button>
-      </Link>
-      {pokemon.map((p) => (
-        <div key={p.id}>
-          <h2>{p.name.english}</h2>
-          <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`}
-            alt={p.name.english}
-          />
-          <Link to={`/PokemonDetails/${p.id}`}>
-            <button>Show Details</button>
+    <>
+      <h1>Welcome to Pokémon Fight!</h1>
+      <h2>Choose your Pokémon and go to the BattleGame page to fight</h2>
+
+      {/* <div className="battlegame_button_div">
+          <Link to={`/BattleGame`}>
+            <button className="battlegame_button">Go to BattleGame page</button>
           </Link>
-        </div>
-      ))}
-    </div>
+        </div> */}
+      <div className="home_body">
+        {pokemon.map((p) => (
+          <div key={p.id} className="pokemon">
+            <h3>{p.name.english}</h3>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`}
+              alt={p.name.english}
+            />
+            <Link to={`/PokemonDetails/${p.id}`}>
+              <button className="details_button">Show Details</button>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
