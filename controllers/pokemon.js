38e5c0a1971003a1eee10c9c9af647f4e8e4b380
Marie <helleliv@Air-von-Marie.fritz.box>
2023-07-06 11:41:45 +0200

    models

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,55 +1,66 @@
-const jsonData = require('../pokedex.json');
+const jsonData = require("../pokedex.json");
+const Result = require("../models/results");
 
 exports.getAllPokemon = (req, res) => {
-    res.json(jsonData);
+  res.json(jsonData);
 };
 
 exports.getPokemonById = (req, res) => {
-    const id = req.params.id;
-    const pokemon = jsonData.find(p => p.id === parseInt(id));
+  const id = req.params.id;
+  const pokemon = jsonData.find((p) => p.id === parseInt(id));
 
-    if (pokemon) {
-        res.json(pokemon);
-    } else {
-        res.status(404).json({ error: 'Pokemon not found' });
-    }
-
+  if (pokemon) {
+    res.json(pokemon);
+  } else {
+    res.status(404).json({ error: "Pokemon not found" });
+  }
 };
 
 exports.getPokemonByIdAndName = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    const pokemon = jsonData.find(p => p.id === parseInt(id));
+  const pokemon = jsonData.find((p) => p.id === parseInt(id));
 
-    if (pokemon) {
-
-        res.json(pokemon.name);
-    } else {
-        res.status(404).json({ error: 'Pokemon not found' });
-    }
+  if (pokemon) {
+    res.json(pokemon.name);
+  } else {
+    res.status(404).json({ error: "Pokemon not found" });
+  }
 };
 
 exports.getPokemonByIdAndType = (req, res) => {
-    const id = req.params.id;
-    const pokemon = jsonData.find(p => p.id === parseInt(id));
+  const id = req.params.id;
+  const pokemon = jsonData.find((p) => p.id === parseInt(id));
 
-    if (pokemon) {
-        res.json(pokemon.type);
-    } else {
-        res.status(404).json({ error: 'Pokemon not found' });
-    }
+  if (pokemon) {
+    res.json(pokemon.type);
+  } else {
+    res.status(404).json({ error: "Pokemon not found" });
+  }
 };
 
 exports.getPokemonByIdAndBase = (req, res) => {
-    const id = req.params.id;
-    const pokemon = jsonData.find(p => p.id === parseInt(id));
+  const id = req.params.id;
+  const pokemon = jsonData.find((p) => p.id === parseInt(id));
 
-    if (pokemon) {
-        res.json(pokemon.base);
-    } else {
-        res.status(404).json({ error: 'Pokemon not found' });
-    }
+  if (pokemon) {
+    res.json(pokemon.base);
+  } else {
+    res.status(404).json({ error: "Pokemon not found" });
+  }
 };
 
+exports.saveGameResults = async (req, res) => {
+  try {
+    const { player1, player2, winner } = req.body;
 
-
+    const gameResult = await Result.create({
+      player1,
+      player2,
+      winner,
+    });
+    res.status(201).json(gameResult);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express')
-let jsonData = require('./pokedex.json');
-const app = express()
-const port = 3000
+const express = require("express");
+require("dotenv/config");
+require("./db.js");
+let jsonData = require("./pokedex.json");
+const app = express();
+const port = 3000;
 const cors = require("cors");
-const pokemonRouter = require('./routes/pokemon');
+const pokemonRouter = require("./routes/pokemon");
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/', pokemonRouter);
-
+app.use("/", pokemonRouter);
+app.use("/game/save", pokemonRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
